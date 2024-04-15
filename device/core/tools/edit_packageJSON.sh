95ee354de9f0a6b39041a4f97233fc5081eddb0b
TICO-79009 <takashi.matsuda@mail.toyota-shokki.co.jp>
2024-04-15 06:35:56 +0000

    fix edit_packageJSON.sh

--- device/core/tools/edit_packageJSON.sh
+++ device/core/tools/edit_packageJSON.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 name="@project-gaudi/gaudi-iot-device"
-version={$VERSION}
+version=${VERSION}
 description="Based on Azure IoT device SDK v1.18.2"
 
 target="device/core/package.json"
 
-sed -i 's/\"name\": \"azure-iot-device\",/\"name\": \"'"${name}"'\",/g' "${target}"
-sed -i 's/\"version\": \".*\",/\"version\": \"'"${version}"'\",/g' "${target}"
-sed -i 's/\"description\": \".*\",/\"description\": \"'"${description}"'\",/g' "${target}"
+sed -i 's| "name": "azure-iot-device",| "name": "'"${name}"'", |g' "${target}"
+sed -i 's| "version": ".*",| "version": "'"${version}"'", |g' "${target}"
+sed -i 's| "description": ".*",| "description": "'"${description}"'", |g' "${target}"
