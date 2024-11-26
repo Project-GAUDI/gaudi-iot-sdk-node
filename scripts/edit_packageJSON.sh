bdd4e69f45a03211f695264ed37b84143efe16ae
TICO-79009 <takashi.matsuda@mail.toyota-shokki.co.jp>
2024-11-26 10:37:26 +0000

    Fix GitHub Actions

--- scripts/edit_packageJSON.sh
+++ scripts/edit_packageJSON.sh
@@ -4,7 +4,7 @@ name="@project-gaudi/${NAME}"
 version=${VERSION}
 author="Toyota Industries Corporation"
 url="https://github.com/Project-GAUDI/gaudi-iot-device"
-target=${PATH}
+target=${TARGET}
 
 # 元バージョン抜き出し
 baseVersion=`grep "version" ${target} | sed 's/.*\([0-9]\+\.[0-9]\+\.[0-9]\+\).*/\1/g'`
@@ -20,7 +20,8 @@ description="Based on ${baseDiscription} v${baseVersion}"
 sed -i 's/\"name\": \"'"${baseName}"'\",/\"name\": \"'"${name}"'\",/g' "${target}"
 sed -i 's/\"version\": \".*\",/\"version\": \"'"${version}"'\",/g' "${target}"
 sed -i 's/\"description\": \".*\",/\"description\": \"'"${description}"'\",/g' "${target}"
-sed -i 's/\"azure-iot-amqp-base\": \".*\",/\"gaudi-iot-amqp-base\": \"'"${version}"'\",/g' "${target}"
 sed -i 's/\"azure-iot-device\": \".*\",/\"gaudi-iot-device\": \"'"${version}"'\",/g' "${target}"
+sed -i 's/\"azure-iot-amqp-base\": \".*\",/\"gaudi-iot-amqp-base\": \"'"${version}"'\",/g' "${target}"
+sed -i 's/\"azure-iot-device-amqp\": \".*\",/\"gaudi-iot-device-amqp\": \"'"${version}"'\",/g' "${target}"
 sed -i 's| "author": ".*",| "author": "'"${author}"'", |g' "${target}"
 sed -i 's|https://github.com/Azure/azure-iot-sdk-node|'"${url}"'|g' "${target}"
