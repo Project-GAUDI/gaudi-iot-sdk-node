c14d355bcf06c087ca5d173c93d0fd36b78b333e
TICO-79009 <takashi.matsuda@mail.toyota-shokki.co.jp>
2024-11-26 09:10:41 +0000

    Changed with upgrade to v6.0.1

---
+++ release-notes.md
@@ -0,0 +1,21 @@
+# gaudi-iot-sdk-node Release Notes
+
+### 6.0.0
+
+2024-07-31
+
+* typescriptバージョン更新(4.4.4→4.9.5)(全カスタムパッケージ)
+* 新大容量メッセージ対応
+    * メッセージサイズを大きくした際のメッセージ受信遅延の解消
+        * Amqpライブラリのrheaのバージョン変更(\^1.0.15→\^3.0.1)(azure-iot-amqp-base・azure-iot-device-amqp)
+        * azure-iot-amqp-base(v2.5.2)をベースにgaudi-iot-amqp-baseを新規作成
+        * azure-iot-device-amqp(v1.14.2)をベースにgaudi-iot-device-amqpを新規作成
+        * 新規カスタムSDKをビルドできるようにパイプライン修正
+
+### 1.0.0
+
+2023-08-04
+
+* azure-iot-device(v1.18.2)をベースにgaudi-iot-deviceを新規作成
+* タイムアウト時間を設定できるように修正(gaudi-iot-device)
+* 再接続不要判定時、再接続失敗時に発行するイベント追加(gaudi-iot-device)
---
+++ scripts/edit_packageJSON.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+name="@project-gaudi/${NAME}"
+version=${VERSION}
+author="Toyota Industries Corporation"
+url="https://github.com/Project-GAUDI/gaudi-iot-device"
+
+# 元バージョン抜き出し
+baseVersion=`grep "version" ${target} | sed 's/.*\([0-9]\+\.[0-9]\+\.[0-9]\+\).*/\1/g'`
+
+# 元の名前抜き出し
+baseName=`grep "name" ${target} | sed 's/.*: \"\(.*\)\",/\1/g'`
+
+# 元のdescription抜き出し
+baseDiscription=`grep "description" ${target} | sed 's/.*: \"\(.*\)\",/\1/g'`
+# 元のdescription抜き出し+元バージョン
+description="Based on ${baseDiscription} v${baseVersion}"
+
+sed -i 's/\"name\": \"'"${baseName}"'\",/\"name\": \"'"${name}"'\",/g' "${target}"
+sed -i 's/\"version\": \".*\",/\"version\": \"'"${version}"'\",/g' "${target}"
+sed -i 's/\"description\": \".*\",/\"description\": \"'"${description}"'\",/g' "${target}"
+sed -i 's/\"azure-iot-amqp-base\": \".*\",/\"gaudi-iot-amqp-base\": \"'"${version}"'\",/g' "${target}"
+sed -i 's/\"azure-iot-device\": \".*\",/\"gaudi-iot-device\": \"'"${version}"'\",/g' "${target}"
+sed -i 's| "author": ".*",| "author": "'"${author}"'", |g' "${target}"
+sed -i 's|https://github.com/Azure/azure-iot-sdk-node|'"${url}"'|g' "${target}"
